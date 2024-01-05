4fc99a92dcf71a4770a412584bbc4f6988adc492
Benedikt Holland <benedikt.holland@protonmail.com>
2024-01-05 18:36:58 +0100

    ✨ implement rapidfire

--- simulate.c
+++ simulate.c
@@ -5,47 +5,30 @@
 #include <stdbool.h>
 #include "simulate.h"
 
-//TODO Find out why results do not match online simulator
-// https://github.com/jstar88/opbe
-
-// LJ, SJ, Xer, SS, Sxer, BB, Zerri, RIP, RAPIDFIRE, PF, KT, GT, Kolo, Rec, Spio
-const int HULL[] = {400, 1000, 2700, 6000, 7000, 7500, 11000, 900000, 14000, 2300, 400, 1200, 3000, 1600, 100};
-const int SHIELD[] = {10, 25, 50, 200, 400, 500, 500, 50000, 700, 100, 10, 25, 100, 10, 0};
-const int DMG[] = {50, 150, 400, 1000, 700, 1000, 2000, 200000, 2800, 200, 5, 5, 50, 1, 0};
-const int NUM_SHIP_TYPES = 15;
-const int RAPIDFIRE[NUM_SHIP_TYPES][NUM_SHIP_TYPES];
-RAPIDFIRE[0] =  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}; // LJ
-RAPIDFIRE[1] =  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 5}; // SJ
-RAPIDFIRE[2] =  {6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}; // Xer
-RAPIDFIRE[3] =  {0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 5}; // SS
-RAPIDFIRE[4] =  {0, 4, 4, 7, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 5}; // Sxer
-RAPIDFIRE[5] =  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}; // BB
-RAPIDFIRE[6] =  {0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}; // Zerri
-RAPIDFIRE[7] =  {200, 100, 33, 30, 15, 25, 5, 0, 10, 30, 250, 250, 250, 250, 1250}; // RIP
-RAPIDFIRE[8] =  {0, 0, 0, 7, 0, 4, 3, 0, 0, 0, 0, 0, 0, 0, 5}; // RP
-RAPIDFIRE[9] =  {3, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}; // PF
-RAPIDFIRE[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}; // KT
-RAPIDFIRE[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}; // GT
-RAPIDFIRE[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}; // Kolo
-RAPIDFIRE[13] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}; // Rec
-RAPIDFIRE[14] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // Spio
-
 int main() {
-    int size1 = 30000;
-    int fleet1[size1*3];
-    for (int i=0; i<size1*3; i+=3) {
-        fleet1[i] = 2;
-        fleet1[i+1] = HULL[fleet1[i]];
-        fleet1[i+2] = SHIELD[fleet1[i]];
+    int attacker_fleet[15] = {0};
+    int defender_fleet[15] = {0};
+    attacker_fleet[6] = 70000;
+    defender_fleet[7] = 1000;
+    int attacker_size = 0;
+    int defender_size = 0;
+    for (int type=0;type<NUM_SHIP_TYPES;type++) {
+        attacker_size += attacker_fleet[type];
+        defender_size += defender_fleet[type];
     }
-    int size2 = 13000;
-    int fleet2[size2*3];
-    for (int i=0; i<size2*3; i+=3) {
-        fleet2[i] = 3;
-        fleet2[i+1] = HULL[fleet2[i]];
-        fleet2[i+2] = SHIELD[fleet2[i]];
+    Subfleet * attacker = (Subfleet*)malloc(NUM_SHIP_TYPES * sizeof(Subfleet));
+    Subfleet * defender = (Subfleet*)malloc(NUM_SHIP_TYPES * sizeof(Subfleet));
+    for (int type=0;type<NUM_SHIP_TYPES;type++) {
+        Subfleet * att_subfleet = attacker+type;
+        Subfleet * def_subfleet = defender+type;
+        att_subfleet->ship = NULL;
+        att_subfleet->size = attacker_fleet[type];
+        att_subfleet->exists = att_subfleet->size > 0;
+        def_subfleet->ship = NULL;
+        def_subfleet->size = defender_fleet[type];
+        def_subfleet->exists = def_subfleet->size > 0;
     }
-    int winner = simulate_battle(fleet1, size1, fleet2, size2);
+    int winner = simulate_battle(attacker, attacker_size, defender, defender_size, SHIP_STATS);
     switch (winner) {
         case 0: printf("Draw"); break;
         case 1: printf("Attacker wins"); break;
@@ -55,16 +38,15 @@ int main() {
     return 1;
 }
 
-int simulate_battle(int *attacker, int att_size, int *defender, int def_size) {
+int simulate_battle(Subfleet * attacker, int attacker_size, Subfleet * defender, int defender_size, int stats[3][NUM_SHIP_TYPES]) {
     srand(time(NULL));
-    Fleet * att_fleet = construct_fleet(attacker, att_size);
-    Fleet * def_fleet = construct_fleet(defender, def_size);
+    Fleet * att_fleet = construct_fleet(attacker, attacker_size, stats);
+    Fleet * def_fleet = construct_fleet(defender, defender_size, stats);
     for (int round=0;round<6;round++) {
-        Fleet * def_copy = (Ship*)malloc(def_size * sizeof(Ship));
-        memcpy(&def_copy, &def_fleet, sizeof(def_fleet));
-        simulate_round(att_fleet, def_fleet);
-        simulate_round(def_copy, att_fleet);
-        printf("Round: %d, Attacker: %d, Defender: %d\n", round+1, att_size, def_size);
+        Fleet * def_copy = construct_fleet(defender, def_fleet->size, stats);
+        simulate_round(att_fleet, def_fleet, stats);
+        simulate_round(def_copy, att_fleet, stats);
+        printf("Round: %d, Attacker: %d, Defender: %d\n", round+1, att_fleet->size, def_fleet->size);
         if (att_fleet->size != 0 && def_fleet->size == 0) {
             return 1;
         } else if (att_fleet->size == 0 && def_fleet->size != 0) {
@@ -76,86 +58,100 @@ int simulate_battle(int *attacker, int att_size, int *defender, int def_size) {
     return 0;
 }
 
-Fleet * construct_fleet(int * fleet_spec, int fleet_size) {
-    // Initialize subfleets
-    Subfleet * subfleets = (Subfleet*)malloc(NUM_SHIP_TYPES*sizeof(Subfleet));
-    for (int i=0; i<NUM_SHIP_TYPES;i++) { 
-        Subfleet * subfleet = subfleets+i;
-        subfleet->exists = false;
-    }
-    // Initiliaze ships in fleet
-    Ship * ships = (Ship*)malloc(fleet_size * sizeof(Ship));
-    for (int i=0; i<fleet_size;i++) {
-        Ship * ship = ships+i;
-        int ship_type = *(fleet_spec+3*i);
-        ship->type = ship_type;
-        ship->hull = *(fleet_spec+3*i+1);
-        ship->shield = *(fleet_spec+3*i+2);
-        Subfleet * subfleet = subfleets+ship_type;
-        if (!subfleet->exists) {
-            subfleet->exists = true;
-            subfleet->ship = ship;
-            subfleet->size = 1;
-        } else {
-            subfleet->size += 1;
+Fleet * construct_fleet(Subfleet * subfleets, int fleet_size, int stats[3][NUM_SHIP_TYPES]) {
+    Ship * head = NULL;
+    Ship * current_ship = NULL;
+    for (int type=0;type<NUM_SHIP_TYPES;type++) {
+        Subfleet * subfleet = subfleets+type;
+        for (int i=0;i<subfleet->size;i++) {
+            Ship * new_ship = (Ship*)malloc(sizeof(Ship));
+            if (subfleet->ship == NULL) {
+                subfleet->ship = new_ship;
+            }
+            new_ship->prev = current_ship;
+            new_ship->next = NULL;
+            if (current_ship != NULL) current_ship->next = new_ship;
+            new_ship->type = type;
+            new_ship->hull = stats[HULL][type];
+            new_ship->shield = stats[SHIELD][type];
+            current_ship = new_ship;
+            if (head == NULL) head = current_ship;
         }
     }
     // Pack together into single struct
     Fleet * fleet = (Fleet*)malloc(sizeof(Fleet));
-    fleet->fleet = ships;
+    fleet->fleet = head;
     fleet->subfleets = subfleets;
     fleet->size = fleet_size;
     return fleet;
 }
 
-void fire(Ship * ship, int target_idx, Fleet * def_fleet) {
-    Ship * target = def_fleet+target_idx;
-    int shield = target->shield - DMG[ship->type];
-    if (target->shield == 0 || (float)DMG[ship->type] / target->shield > 0.01) {
+void fire(Ship * ship, int target_idx, Fleet * def_fleet, int stats[3][NUM_SHIP_TYPES]) {
+    Ship * target = def_fleet->fleet;
+    for (int i=0;i<target_idx-1;i++) {
+        target = target->next;
+    }
+    int shield = target->shield - stats[DMG][ship->type];
+    if (target->shield == 0 || (float)stats[DMG][ship->type] / target->shield > 0.01) {
         if (shield < 0) {
             target->hull += shield;
             shield = 0;
         }
         target->shield = shield;
     }
-    float hull_rem = (float)target->hull / HULL[target->type];
-    if (hull_rem < 0.7 && (float)rand()/(float)RAND_MAX <= 1-hull_rem) {
-        def_fleet->size--;
-        if (def_fleet->size == 0) {
-            return;
+    float hull_rem = (float)target->hull / stats[HULL][target->type];
+    if (hull_rem < 0.7 && (double)rand() / RAND_MAX < (double)1-hull_rem) {
+        // Decrement and potentially delete subfleet
+        Subfleet * fleet_of_type = def_fleet->subfleets+target->type;
+        fleet_of_type->size--;
+        if (fleet_of_type->size <= 0) {
+            fleet_of_type->exists = false;
         }
-        // Move last element of subfleet to destroyed ship
-        // Decrement subfleet->size --
-        // How to fill the hole? Move all last elements from subw
-        // Linked list? Downside, have to iterate through list 
-        // Potentially destroy subfleet (exists=false)
-        *(def_fleet+target_idx) = *(def_fleet+def_fleet->size);
+        // Remove destroyed ship from fleet
+        if (def_fleet->size > 1) {
+            if (target->prev == NULL) {
+                target->next->prev = NULL;
+                def_fleet->fleet = target->next;
+            } else if (target->next == NULL) {
+                target->prev->next = NULL;
+            } else {
+                Ship * prev_ship = target->prev;
+                target->prev->next = target->next;
+                target->next->prev = prev_ship;
+            }
+        }
+        free(target);
+        // Decrement fleet size
+        def_fleet->size--;
     }
 }
 
-void simulate_round(Fleet * att_fleet, Fleet * def_fleet) {
-    for (int att_idx=0;att_idx<att_fleet->size;att_idx++) {
-        Ship * att_ship = att_fleet+att_idx;
-        int target_idx= rand_lim(def_fleet->size);
-        fire(att_ship, target_idx, def_fleet);
+void simulate_round(Fleet * att_fleet, Fleet * def_fleet, int stats[3][NUM_SHIP_TYPES]) {
+    Ship * att_ship = att_fleet->fleet;
+    while (att_ship != NULL) {
+        int target_idx= rand() % def_fleet->size;
+        fire(att_ship, target_idx, def_fleet, stats);
+        if (def_fleet->size <= 0) return;
         // Rapidfire
         for (int i=0; i<NUM_SHIP_TYPES; i++) {
             int rapidfire = RAPIDFIRE[att_ship->type][i];
             Subfleet * fleet_of_type = def_fleet->subfleets+i;
             if (rapidfire > 0 && fleet_of_type->exists) {
                 // Need a map of how many of each ship types are left
-                while ((rapidfire-1)/rapidfire <= (float)rand()/(float)RAND_MAX) {
-                    int rapidfire_target_idx = rand_lim(fleet_of_type->size);
-                    Ship * rapidfire_target = fleet_of_type->ship+rapidfire_target_idx;
-                    fire(att_ship, rapidfire_target, def_fleet);
+                while ((double)rand() / RAND_MAX < (double)(rapidfire-1)/rapidfire) {
+                    int rapidfire_target_idx = rand() % fleet_of_type->size;
+                    fire(att_ship, rapidfire_target_idx, def_fleet, stats);
+                    if (def_fleet->size <= 0) return;
                 }
             }
         }
-    }   
+        att_ship = att_ship->next;
+    } 
     // Recharge shields
-    for (int def_idx=0; def_idx<def_fleet->size; def_idx++) {
-        Ship * ship = def_fleet+def_idx;
-        ship->shield=SHIELD[ship->type];
+    Ship * def_ship = def_fleet->fleet;
+    while (def_ship != NULL) {
+        def_ship->shield=stats[SHIELD][def_ship->type];
+        def_ship = def_ship->next;
     }
 }
 
--- simulate.h
+++ simulate.h
@@ -1,9 +1,38 @@
+#define NUM_SHIP_TYPES 15
+#define HULL 0
+#define SHIELD 1
+#define DMG 2
+// LJ, SJ, Xer, SS, Sxer, BB, Zerri, RIP, RAPIDFIRE, PF, KT, GT, Kolo, Rec, Spio
+int SHIP_STATS[3][NUM_SHIP_TYPES] = {
+    {400, 1000, 2700, 6000, 7000, 7500, 11000, 900000, 14000, 2300, 400, 1200, 3000, 1600, 100}, //Hull
+    {10, 25, 50, 200, 400, 500, 500, 50000, 700, 100, 10, 25, 100, 10, 0}, // Shield
+    {50, 150, 400, 1000, 700, 1000, 2000, 200000, 2800, 200, 5, 5, 50, 1, 0} // Damage
+};
+const int RAPIDFIRE[NUM_SHIP_TYPES][NUM_SHIP_TYPES] = {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}, // LJ
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 5}, // SJ
+    {6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}, // Xer
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 5}, // SS
+    {0, 4, 4, 7, 0, 0, 0, 0, 0, 0, 3, 3, 0, 0, 5}, // Sxer
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}, // BB
+    {0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}, // Zerri
+    {200, 100, 33, 30, 15, 25, 5, 0, 10, 30, 250, 250, 250, 250, 1250}, // RIP
+    {0, 0, 0, 7, 0, 4, 3, 0, 0, 0, 0, 0, 0, 0, 5}, // RP
+    {3, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}, // PF
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}, // KT
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}, // GT
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}, // Kolo
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5}, // Rec
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} // Spio
+};
+
 // Single ship within a fleet
 typedef struct Ship {
     unsigned short int type;
     int hull;
     int shield;
     struct Ship * next;
+    struct Ship * prev;
 } Ship;
 
 // Keeps track of each type of ships in the fleet
@@ -19,8 +48,8 @@ typedef struct Fleet {
     struct Subfleet * subfleets;
 } Fleet;
 
-int simulate_battle(int *attacker, int att_size, int *defender, int def_size);
-struct Fleet * construct_fleet(int * fleet_spec, int fleet_size);
-void fire(struct Ship * ship, int target_idx, struct Fleet * def_fleet) ;
-void simulate_round(struct Fleet * att_fleet, struct Fleet * def_fleet);
+int simulate_battle(Subfleet * attacker, int attacker_size, Subfleet * defender, int defender_size, int stats[3][NUM_SHIP_TYPES]);
+Fleet * construct_fleet(Subfleet * subfleets, int fleet_size, int stats[3][NUM_SHIP_TYPES]);
+void fire(Ship * ship, int target_idx, Fleet * def_fleet, int stats[3][NUM_SHIP_TYPES]);
+void simulate_round(Fleet * att_fleet, Fleet * def_fleet, int stats[3][NUM_SHIP_TYPES]);
 int rand_lim(int limit);
